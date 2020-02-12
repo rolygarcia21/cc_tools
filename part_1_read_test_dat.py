ab57280bb297f9833786f3692803c8c7fd0e572c
rolygarcia21 <47752832+rolygarcia21@users.noreply.github.com>
2020-02-12 00:02:23 -0500

    Added Part 1 deliverables

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -5,4 +5,7 @@ input_dat_file = "data/pfgd_test.dat"
 
 
 #Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
+level_pack = cc_dat_utils.make_cc_level_pack_from_dat(input_dat_file)
+
 #print the resulting data
+print(level_pack)
